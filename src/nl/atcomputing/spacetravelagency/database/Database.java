653c4896474e6646d08ee1a28302e99b298f9c5a
Martijn Brekhof <m.brekhof@gmail.com>
2015-09-22 12:08:32 +0200

    Added license and copyright statements

--- src/nl/atcomputing/spacetravelagency/database/Database.java
+++ src/nl/atcomputing/spacetravelagency/database/Database.java
@@ -1,3 +1,24 @@
+/**
+ * 
+ * Copyright 2015 AT Computing BV
+ *
+ * This file is part of Space Travel Agency.
+ *
+ * Space Travel Agency is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Space Travel Agency is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Space Travel Agency.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package nl.atcomputing.spacetravelagency.database;
 
 import android.content.ContentValues;
